9fef70655d62088d9bc8daa3be560bbf3219cd8b
Viva <viva@slide.com>
2010-09-01 23:04:07 -0700

    dev configs

--- app/include/config.php
+++ app/include/config.php
@@ -1,22 +1,23 @@
 <?php
-// $facebook_xd_url = '149641118398720';
+$web_url = "http://www.adcontests.com/svedka/app";
 
 // dev
-//$facebook_app_id = '103646963029008';
-//$facebook_api_key = '3e0979345f249c435372efa9d8f514ca';
-//$facebook_secret_key = 'c86e917f3ac6ebc29e17560ed4bc01da';
+$facebook_app_id = '103646963029008';
+$facebook_api_key = '3e0979345f249c435372efa9d8f514ca';
+$facebook_secret_key = 'c86e917f3ac6ebc29e17560ed4bc01da';
+$like_app_id = '153004184716221';  // App Contest Page
 
 // production
-$facebook_app_id = '103646963029008';
-$facebook_api_key = '89908958ac1940173e8888ebd3dc16f1';
-$facebook_secret_key = 'c9ba1c23c11f8b28c33bb69d0b3fd641';
-
+//$facebook_app_id = '103646963029008';
+//$facebook_api_key = '89908958ac1940173e8888ebd3dc16f1';
+//$facebook_secret_key = 'c9ba1c23c11f8b28c33bb69d0b3fd641';
+//$like_app_id = "60649471874";  // Svedka Page
 
 // dev
 $app_url = "http://dev.adcontests.com";
 
 // production
-$app_url = "http://www.adcontests.com/svedka/app";
+//$app_url = "http://www.adcontests.com/svedka/app";
 
 
 
