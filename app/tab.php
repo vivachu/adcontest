af4183d250230604e58767d54e2ecbc86ce10745
root <root@ip-10-202-199-148.ec2.internal>
2010-09-03 20:33:05 -0400

    like

--- app/tab.php
+++ app/tab.php
@@ -16,12 +16,7 @@
 	}
 
 	function share() {
-		 var share = {
-		   method: 'stream.share',
-		   u: '<?= $app_url ?>'
-		 };
-
-		 FB.ui(share, function(response) { console.log(response); });
+	new Dialog().showMessage('Dialog', 'Hello World.');
 	}
 
 	function like() {
@@ -30,41 +25,23 @@
 
 	function play() {
  		var isFan = false;
- 	<fb:fbml version="1.1">
-	   <fb:visible-to-connection>
-		isFan = true;
-	   </fb:visible-to-connection>
-	</fb:fbml>
 		if (isFan) {
 			document.location = "https://graph.facebook.com/oauth/authorize?client_id=<?= $facebook_app_id ?>&redirect_uri=<?= $app_url ?>/&scope=email,publish_stream,user_birthday,user_likes";
 		} else {
-			document.getElementById("likePopup").style.display="block";
-		}
+			document.getElementById("likePopup").setStyle("display","block");
+		}		
 	}
-
+	document.getElementById('playLink').addEventListener('click', play);
 	document.getElementById('shareLink').addEventListener('click', share);
 	document.getElementById('inviteFriendsLink').addEventListener('click', inviteFriends);
 //--></script>
 <link rel="stylesheet" type="text/css" href="<?= $web_url ?>/reset.css" />
-<link rel="stylesheet" type="text/css" href="<?= $web_url ?>/style_520.php?v=1.1" />
-<script src="http://connect.facebook.net/en_US/all.js"></script>
+<link rel="stylesheet" type="text/css" href="<?= $web_url ?>/style_520.php?v=1.2" />
 
 <!--[if lt IE 8]>
 <link rel="stylesheet" type="text/css" href="<?= $web_url ?>/ie.css" />
 <![endif]-->
 	<div id="fb-root"></div>
-	<script><!--
-	  window.fbAsyncInit = function() {
-		FB.init({appId: '<?= $facebook_app_id ?>', status: true, cookie: true,
-				 xfbml: true});
-	  };
-	  (function() {
-		var e = document.createElement('script'); e.async = true;
-		e.src = document.location.protocol +
-		  '//connect.facebook.net/en_US/all.js';
-		document.getElementById('fb-root').appendChild(e);
-	  }());
-//-->	</script>
 	<div id="container2">
     	<div id="top">
         	<p class="left">Like us? Click the button above.</p>
@@ -80,13 +57,13 @@
 	<!-- Static HTML landing page -->
 				<p><b font="Arial">Everyone is a winner!</b> Sort of. Just pick a door to see if you win this week's amazing SVEDKA BOT prize or end up with a fun consolation NOT prize. <b font="Arial">Increase your chances to win</b> by inviting friends. If one of them wins a Bot Grand Prize, you do too! Click below to play.  <span style="font-size:10px;"><a target="_blank" href="<?=$web_url?>/rules.php" style="color:#ffc821;">See Official Rules</a> for details.</span></p>
 				<p class="title">This week's bot prize: <span><?= $grandPrize['name'] ?></span></p>
-				<a id="playLink" href="#" onclick="play();" class="playBtn">play</a>
+				<a id="playLink" href="#" onclick="return false" class="playBtn">play</a>
 				<div class="bot" style="top:240px;"><img src="<?= $web_url ?>/images/bot2.png" alt="" /></div>
 				<img src="<?= $web_url ?>/images/door2.png" alt="" />
-				<div id="likePopup" class="popup" style="display:none;">
+				<div id="likePopup" class="popup" style="display:block;">
 					<h3>I "Like" SVEDKA Vodka</h3>
 					<p>To win a "BOT or NOT?" prize, you need to click the "Like" button at the top of the page. Swedish imported, five times distilled and a chance to win amazing BOT prizes ... what's not to like?</p>
-					<a href="#" class="right" onclick="document.getElementById('likePopup').style.display='none';">close</a>
+					<a href="#" class="right" onclick="document.getElementById('likePopup').setStyle('display', 'none'); return false;">close</a>
 				</div>
 				<div id="alreadyPlayedPopup" class="popup" style="display:none;">
 					<h3>Play Responsibly</h3>
