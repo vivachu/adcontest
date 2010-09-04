78974a0dd9eac92caa8961a6a2f12e386b3c2720
Viva <viva@slide.com>
2010-09-04 13:06:56 -0700

    share url

--- app/include/config.php
+++ app/include/config.php
@@ -2,7 +2,7 @@
 //$fan_page_url = "http%3A%2F%2Fwww.facebook.com%2Fsvedka&t=Svedka%20BOT%20or%20NOT";
 //$fan_page_url = "http%3A%2F%2Fwww.facebook.com%2Fpages%2FAd-Contest%2F153004184716221%3Fv%3Dapp_103646963029008";
 $fan_page_url = "http://www.facebook.com/pages/Ad-Contest/153004184716221?v=app_103646963029008";
-$share_url = "http%3A%2F%2Fwww.adcontests.com%2Fsvedka%2Fapp%2Ffriend.php";
+$share_url = "http://www.adcontests.com/svedka/app/friend.php";
 
 $web_url = "http://dev.adcontests.com";
 //$web_url = "http://www.adcontests.com/svedka/app";
