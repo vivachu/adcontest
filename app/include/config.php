2b60c7913e43730620f0b32a36e222d0fafc0d98
Viva <viva@slide.com>
2010-09-06 10:27:52 -0700

    pupung

--- app/include/config.php
+++ app/include/config.php
@@ -2,28 +2,29 @@
 //$fan_page_url = "http%3A%2F%2Fwww.facebook.com%2Fsvedka&t=Svedka%20BOT%20or%20NOT";
 //$fan_page_url = "http%3A%2F%2Fwww.facebook.com%2Fpages%2FAd-Contest%2F153004184716221%3Fv%3Dapp_103646963029008";
 $fan_page_url = "http://www.facebook.com/pages/Ad-Contest/153004184716221?v=app_103646963029008";
-$share_url = "http://www.adcontests.com/svedka/app/friend.php";
+//$share_url = "http://www.adcontests.com/svedka/app/friend.php";
+$share_url = "http://dev.adcontests.com/friend.php";
 
-//$web_url = "http://dev.adcontests.com";
-$web_url = "http://www.adcontests.com/svedka/app";
-
-// dev
-//$app_url = "http://apps.facebook.com/bot-or-not-dev";
-// production
-$app_url = "http://apps.facebook.com/bot-or-not";
-
+$web_url = "http://dev.adcontests.com";
+//$web_url = "http://www.adcontests.com/svedka/app";
 
 // dev
-//$facebook_app_id = '103646963029008';
-//$facebook_api_key = '3e0979345f249c435372efa9d8f514ca';
-//$facebook_secret_key = 'c86e917f3ac6ebc29e17560ed4bc01da';
-//$like_app_id = '153004184716221';  // App Contest Page
-
+$app_url = "http://apps.facebook.com/bot-or-not-dev";
 // production
+//$app_url = "http://apps.facebook.com/bot-or-not";
+
+
+// dev
 $facebook_app_id = '103646963029008';
-$facebook_api_key = '89908958ac1940173e8888ebd3dc16f1';
-$facebook_secret_key = 'c9ba1c23c11f8b28c33bb69d0b3fd641';
-$like_app_id = "60649471874";  // Svedka Page
+$facebook_api_key = '3e0979345f249c435372efa9d8f514ca';
+$facebook_secret_key = 'c86e917f3ac6ebc29e17560ed4bc01da';
+$like_app_id = '153004184716221';  // App Contest Page
+
+// production
+//$facebook_app_id = '103646963029008';
+//$facebook_api_key = '89908958ac1940173e8888ebd3dc16f1';
+//$facebook_secret_key = 'c9ba1c23c11f8b28c33bb69d0b3fd641';
+//$like_app_id = "60649471874";  // Svedka Page
 
 $smtp_host = "smtpout.secureserver.net";
 $smtp_port = "80";
