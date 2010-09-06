590c6236524554819e7df3e96ecdf90835457d71
Viva <viva@slide.com>
2010-09-06 10:26:22 -0700

    index file

--- app/redeem.php
+++ app/redeem.php
@@ -19,7 +19,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Bot Prize Redemption Confirmation</title>
 <link rel="stylesheet" type="text/css" href="reset.css" />
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="style.css?v=1.5" />
 <!--[if lt IE 8]>
 <link rel="stylesheet" type="text/css" href="ie.css" />
 <script src="DD_belatedPNG_0.0.8a.js"></script>
