7a98e3c455a35bf1072ad4d5aa91c66a0cbdc586
Viva <viva@slide.com>
2010-09-09 23:30:49 -0700

    prize schedules

--- database/procedures.sql
+++ database/procedures.sql
@@ -8,16 +8,21 @@ CREATE PROCEDURE schedule_prize(contestId int unsigned, prizePlace int unsigned,
     	DECLARE startDate datetime;
     	DECLARE endDate datetime;
     	DECLARE winDate datetime;
+    	DECLARE i INT DEFAULT 0;
 
-		select start_date, end_date, FROM_UNIXTIME(RAND() * (UNIX_TIMESTAMP(end_date) - UNIX_TIMESTAMP(start_date)) + UNIX_TIMESTAMP(start_date)) into startDate, endDate, winDate from contests where id = contestId;
+		WHILE (i<numWinners) DO
+			select start_date, end_date, FROM_UNIXTIME(RAND() * (UNIX_TIMESTAMP(end_date) - UNIX_TIMESTAMP(start_date)) + UNIX_TIMESTAMP(start_date)) into startDate, endDate, winDate from contests where id = contestId;
 
-		select id into prizeId from prizes where name=prizeName;
-
-    	IF isnull(prizeId) THEN
-			insert into prizes(place, name, value, num_winners, description, image) values(prizePlace, prizeName, prizeValue, numWinners, description, prizeImage);
 			select id into prizeId from prizes where name=prizeName;
-    	END IF;
-		insert into prize_schedule(prize_id, contest_id, win_date) values (prizeId, contestId, winDate);
+
+			IF isnull(prizeId) THEN
+				insert into prizes(place, name, value, num_winners, description, image) values(prizePlace, prizeName, prizeValue, numWinners, description, prizeImage);
+				select id into prizeId from prizes where name=prizeName;
+			END IF;
+			insert into prize_schedule(prize_id, contest_id, win_date) values (prizeId, contestId, winDate);
+			SET i = i + 1;
+		END WHILE;
+
     END;
 |
 
