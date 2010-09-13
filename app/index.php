1c0eccbb425d93d8a95c0feb03fe0f8c30f1a89b
root <root@ip-10-202-199-148.ec2.internal>
2010-09-13 01:00:31 -0400

    fb iframe size

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php
 	require_once 'svedka-config.php';
 ?>
-<fb:iframe src="<?= $web_url ?>/game.php" smartsize="true" frameborder="0" scrolling="no" name="myMainFrame">
+<fb:iframe src="<?= $web_url ?>/game.php?code=<?=$_REQUEST['code'] ?>&fid=<?= $_REQUEST['fid']?>&test_date=<?= $_REQUEST['test_date'] ?>" smartsize="false" width="100%" height="932" frameborder="0" scrolling="no" name="myMainFrame">
 </fb:iframe>
