5007423c23cc32419654eed9e0a73474727b762c
Viva <viva@slide.com>
2010-09-21 23:59:14 -0700

    merged

--- database/create_database.sql
+++ database/create_database.sql
@@ -5,7 +5,8 @@ drop database adcontest;
 create database adcontest;
 grant all privileges on adcontest.* to 'adcontest'@'localhost' identified by 'adcontest' with grant option;
 # only do this if you need to access the db remotely as this is very insecure
-# grant all privileges on adcontest.* to 'adcontest'@'%' identified by 'adcontest' with grant option;
+# grant select privileges on adcontest.* to 'svedka'@'%' identified by 'y3nn1ee1' with grant option;
+
 update mysql.user set execute_priv='Y', super_priv='Y' where user='adcontest';
 grant alter routine on adcontest.* to 'adcontest'@'localhost';
 grant create routine on adcontest.* to 'adcontest'@'localhost';
